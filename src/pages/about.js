5af0bdc3fd23a3ea0d8b765ee1da6f688f0ef3f0
Michael Bonner <michaelwbonner@gmail.com>
2019-11-07 12:00:00 -0700

    extra pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,24 +8,25 @@ function AboutPage() {
     <Layout>
       <SEO
         keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]}
-        title="About"
+        title="About Us"
       />
 
+      <div className="w-full py-16">
+        We're a small group of passionate creators trying to make a difference.
+        We value quality deliverables and human interaction. People are who we
+        are, who we build for, and who we work for.
+      </div>
       <section className="flex flex-col md:flex-row items-center">
         <div className="md:w-2/3 md:mr-8">
           <blockquote className="border-l-4 border-gray-900 font-serif leading-loose pl-4 text-justify">
-            The point is... to live one's life in the full complexity of what
-            one is, which is something much darker, more contradictory, more of
-            a maelstrom of impulses and passions, of cruelty, ecstacy, and
-            madness, than is apparent to the civilized being who glides on the
-            surface and fits smoothly into the world.
+            The only way to make sense out of change is to plunge into it, move
+            with it, and join the dance.
           </blockquote>
 
           <cite className="font-bold mt-4 text-right text-xs uppercase block">
-            – Thomas Nagel
+            – Alan Watts
           </cite>
         </div>
-
         <figure className="w-2/3 md:w-1/3"></figure>
       </section>
     </Layout>
