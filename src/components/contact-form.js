55450e528b8fa1a7396020c34508166fc0676311
Michael Bonner <michaelwbonner@gmail.com>
2019-11-07 15:26:37 -0700

    blue button on contact form

--- src/components/contact-form.js
+++ src/components/contact-form.js
@@ -197,7 +197,7 @@ function ContactForm() {
             </div>
             <div>
               <button
-                className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                className="shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
                 disabled={isSubmitting}
                 type="submit"
               >
