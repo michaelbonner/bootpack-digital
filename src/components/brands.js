ee3d982717fd5e5386f0dd8473d2603dfb7dabd5
Michael Bonner <michaelwbonner@gmail.com>
2019-11-13 14:45:30 -0700

    adjust padding of logos at small res

--- src/components/brands.js
+++ src/components/brands.js
@@ -6,6 +6,16 @@ import NationalEnergyFoundation from "../images/clients/national-energy-foundati
 import Ogio from "../images/clients/ogio-gray.svg";
 import WasatchCovers from "../images/clients/wasatch-covers-gray.svg";
 
+function BrandImage({ title, logo }) {
+  return (
+    <img
+      alt={title}
+      className="w-full max-w-md h-auto md:w-1/3 py-12 px-4 md:p-4 lg:p-8 xl:p-12"
+      src={logo}
+    />
+  );
+}
+
 function Brands() {
   return (
     <div className="mx-16 my-12">
@@ -13,36 +23,15 @@ function Brands() {
         Brands We've Worked With
       </h2>
       <div className="flex flex-wrap justify-center items-center">
-        <img
-          alt="National Energy Foundation"
-          className="w-full max-w-md h-auto md:w-1/3 p-12 md:p-4 lg:p-8 xl:p-12"
-          src={NationalEnergyFoundation}
+        <BrandImage
+          logo={NationalEnergyFoundation}
+          title="National Energy Foundation"
         />
-        <img
-          alt="CocaCola"
-          className="w-full max-w-md h-auto md:w-1/3 p-12 md:p-4 lg:p-8 xl:p-12"
-          src={CocaCola}
-        />
-        <img
-          alt="EDGEHomes"
-          className="w-full max-w-md h-auto md:w-1/3 p-12 md:p-4 lg:p-8 xl:p-12"
-          src={EdgeHomes}
-        />
-        <img
-          alt="Franklin Covey"
-          className="w-full max-w-md h-auto md:w-1/3 p-12 md:p-4 lg:p-8 xl:p-12"
-          src={FranklinCovey}
-        />
-        <img
-          alt="Ogio"
-          className="w-full max-w-md h-auto md:w-1/3 p-12 md:p-4 lg:p-8 xl:p-12"
-          src={Ogio}
-        />
-        <img
-          alt="Wasatch Covers"
-          className="w-full max-w-md h-auto md:w-1/3 p-12 md:p-4 lg:p-8 xl:p-12"
-          src={WasatchCovers}
-        />
+        <BrandImage logo={CocaCola} title="CocaCola" />
+        <BrandImage logo={EdgeHomes} title="EDGEHomes" />
+        <BrandImage logo={FranklinCovey} title="Franklin Covey" />
+        <BrandImage logo={Ogio} title="Ogio" />
+        <BrandImage logo={WasatchCovers} title="Wasatch Covers" />
       </div>
     </div>
   );
