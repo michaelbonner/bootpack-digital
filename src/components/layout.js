330d825b482b7a63ab428f056a9f66328d5bc1fe
Michael Bonner <michaelwbonner@gmail.com>
2019-12-24 11:21:32 -0700

    --ammend

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,15 +9,7 @@ function Layout({ children }) {
     <div className="leading-normal tracking-normal text-gray-900">
       <Header />
 
-      <main
-        className="flex flex-col flex-1 md:justify-center lg:mx-16 px-4 py-8 md:p-8"
-        style={{
-          backgroundImage: `url(${Destinations})`,
-          backgroundSize: `80%`,
-          backgroundPosition: `100% 0%`,
-          backgroundRepeat: `no-repeat`
-        }}
-      >
+      <main className="flex flex-col flex-1 md:justify-center lg:mx-16 px-4 py-8 md:p-8">
         {children}
       </main>
       <Footer />
