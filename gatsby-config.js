74481dcb80cbfec9f6d9283e44f0069b1d581a2d
Michael Bonner <michaelwbonner@gmail.com>
2020-05-12 10:57:23 -0600

    npm deps update

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `Bootpack Digital, LLC`,
     description: `Bootpack Digital provides quality experience for PEOPLE`,
     author: `@bootpack`,
-    siteUrl: `https://bootpackdigital.com`
+    siteUrl: `https://bootpackdigital.com`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -16,26 +16,28 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#4299E1`,
         display: `minimal-ui`,
-        icon: `src/images/bootpack-icon.png`
-      }
+        icon: `src/images/bootpack-icon.png`,
+      },
     },
     {
       resolve: `gatsby-plugin-google-tagmanager`,
       options: {
         id: `GTM-KCZ4PZC`,
         includeInDevelopment: false,
-        defaultDataLayer: { platform: `gatsby` }
-      }
+        defaultDataLayer: { platform: `gatsby` },
+      },
     },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     `gatsby-plugin-sitemap`,
     `gatsby-plugin-postcss`,
     {
       resolve: `gatsby-plugin-purgecss`,
       options: {
         tailwind: true,
-        purgeOnly: [`src/css/style.css`]
-      }
+        purgeOnly: [`src/css/style.css`],
+      },
     },
-    `gatsby-plugin-offline`
-  ]
+    `gatsby-plugin-offline`,
+  ],
 };
