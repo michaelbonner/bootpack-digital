0d3baa6a37c8dcef6c72f4bcb036f1ba9f2af863
Michael Bonner <michaelwbonner@gmail.com>
2020-12-08 17:57:28 -0700

    spacing on li on terms

--- src/pages/terms.js
+++ src/pages/terms.js
@@ -35,33 +35,33 @@ function PrivacyPage() {
               2. Use License
             </h2>
             <ol className="ml-4 list-roman" type="a">
-              <li>
+              <li className="mt-2">
                 Permission is granted to temporarily download one copy of the
                 materials (information or software) on Bootpack Digital, LLC's
                 website for personal, non-commercial transitory viewing only.
                 This is the grant of a license, not a transfer of title, and
                 under this license you may not:
                 <ol className="ml-4 list-decimal" type="i">
-                  <li>modify or copy the materials;</li>
-                  <li>
+                  <li className="mt-2">modify or copy the materials;</li>
+                  <li className="mt-2">
                     use the materials for any commercial purpose, or for any
                     public display (commercial or non-commercial);
                   </li>
-                  <li>
+                  <li className="mt-2">
                     attempt to decompile or reverse engineer any software
                     contained on Bootpack Digital, LLC's website;
                   </li>
-                  <li>
+                  <li className="mt-2">
                     remove any copyright or other proprietary notations from the
                     materials; or
                   </li>
-                  <li>
+                  <li className="mt-2">
                     transfer the materials to another person or "mirror" the
                     materials on any other server.
                   </li>
                 </ol>
               </li>
-              <li>
+              <li className="mt-2">
                 This license shall automatically terminate if you violate any of
                 these restrictions and may be terminated by Bootpack Digital,
                 LLC at any time. Upon terminating your viewing of these
@@ -74,7 +74,7 @@ function PrivacyPage() {
               3. Disclaimer
             </h2>
             <ol className="ml-4 list-roman" type="a">
-              <li>
+              <li className="mt-2">
                 The materials on Bootpack Digital, LLC's website are provided on
                 an 'as is' basis. Bootpack Digital, LLC makes no warranties,
                 expressed or implied, and hereby disclaims and negates all other
@@ -83,7 +83,7 @@ function PrivacyPage() {
                 or non-infringement of intellectual property or other violation
                 of rights.
               </li>
-              <li>
+              <li className="mt-2">
                 Further, Bootpack Digital, LLC does not warrant or make any
                 representations concerning the accuracy, likely results, or
                 reliability of the use of the materials on its website or
