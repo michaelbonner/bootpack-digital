4bf1173a5b622eb388ccb15ffc0be4420f3ab9f5
Michael Bonner <michaelwbonner@gmail.com>
2020-12-08 17:46:04 -0700

    layout on privacy

--- src/pages/privacy.js
+++ src/pages/privacy.js
@@ -10,14 +10,13 @@ function PrivacyPage() {
         description="Do you have a project idea or an existing site that needs to be updated? Do you need an outside consultant? Get in touch!"
         title="Contact Us"
       />
-      <div className="bg-white pt-8">
+      <div className="bg-white py-8">
         <div className="container mx-auto px-4 text-gray-700">
           <h1 className="mt-12 text-3xl leading-9 font-extrabold text-navy-600 sm:text-4xl sm:leading-10">
-            Get in touch
+            Privacy Policy
           </h1>
-          <div className="pt-4 max-w-3xl lg:mx-24 prose">
-            <h2>Privacy Policy</h2>
-            <p>
+          <div className="pt-4 max-w-3xl lg:mx-24">
+            <p className="mt-4">
               Your privacy is important to us. It is Bootpack Digital, LLC's
               policy to respect your privacy regarding any information we may
               collect from you across our website,{` `}
@@ -26,41 +25,43 @@ function PrivacyPage() {
               </a>
               , and other sites we own and operate.
             </p>
-            <p>
+            <p className="mt-4">
               We only ask for personal information when we truly need it to
               provide a service to you. We collect it by fair and lawful means,
               with your knowledge and consent. We also let you know why we’re
               collecting it and how it will be used.
             </p>
-            <p>
+            <p className="mt-4">
               We only retain collected information for as long as necessary to
               provide you with your requested service. What data we store, we’ll
               protect within commercially acceptable means to prevent loss and
               theft, as well as unauthorized access, disclosure, copying, use or
               modification.
             </p>
-            <p>
+            <p className="mt-4">
               We don’t share any personally identifying information publicly or
               with third-parties, except when required to by law.
             </p>
-            <p>
+            <p className="mt-4">
               Our website may link to external sites that are not operated by
               us. Please be aware that we have no control over the content and
               practices of these sites, and cannot accept responsibility or
               liability for their respective privacy policies.
             </p>
-            <p>
+            <p className="mt-4">
               You are free to refuse our request for your personal information,
               with the understanding that we may be unable to provide you with
               some of your desired services.
             </p>
-            <p>
+            <p className="mt-4">
               Your continued use of our website will be regarded as acceptance
               of our practices around privacy and personal information. If you
               have any questions about how we handle user data and personal
               information, feel free to contact us.
             </p>
-            <p>This policy is effective as of 1 December 2020.</p>
+            <p className="mt-4">
+              This policy is effective as of 1 December 2020.
+            </p>
           </div>
         </div>
       </div>
