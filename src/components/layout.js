c1ba43b29b0b5b63d81f394bb00f758aef37f82e
Michael Bonner <michaelwbonner@gmail.com>
2021-03-14 19:47:24 -0600

    load css in layout instead of gatsby-browser

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,8 @@ import React from "react";
 import Header from "./header";
 import Footer from "./footer";
 
+import "../../src/css/style.css";
+
 function Layout({ children }) {
   return (
     <div className="leading-normal tracking-normal text-gray-900">
