a30929a34fd83d23407efb573ce3ee93cbe0366c
Michael Bonner <michaelwbonner@gmail.com>
2021-03-15 16:06:44 -0600

    add missing files

---
+++ src/components/images/work/know-your-force.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { StaticImage } from "gatsby-plugin-image";
+
+export default function KnowYourForce() {
+  return (
+    <StaticImage
+      src="../../../images/work/know-your-force.jpg"
+      alt="Know Your Force"
+    />
+  );
+}
