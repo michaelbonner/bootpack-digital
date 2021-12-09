d82edc312d3935666e7b2a82a08343362f05928c
Michael Bonner <michaelwbonner@gmail.com>
2021-12-09 15:55:24 -0700

    update tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
-  darkMode: false,
+  content: [`./src/**/*.{js,jsx,ts,tsx}`],
   theme: {
     extend: {
       colors: {
@@ -45,9 +44,6 @@ module.exports = {
       decimal: `decimal`,
       roman: `upper-roman`,
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [],
 };
