32635f8a0f84bccd3a460306dcdb54f7a6ba48bb
Michael Bonner <michaelwbonner@gmail.com>
2022-08-01 11:14:11 -0600

    add gtm

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,15 @@
-<script>
+<script lang="ts">
 	import '../app.css';
+
 	import Footer from '../components/footer.svelte';
+	import Gtm from '../components/gtm.svelte';
 	import Header from '../components/header.svelte';
+
+	const gtmId = 'GTM-KCZ4PZC';
+	const gtmDataPoints: string[] = [];
 </script>
+
+<Gtm {gtmId} {gtmDataPoints} />
 
 <div class="leading-normal tracking-normal text-gray-900">
 	<Header />
