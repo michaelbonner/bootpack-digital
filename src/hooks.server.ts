03a38d9b062949549b3214064674fdd52276f559
Michael Bonner <michaelwbonner@gmail.com>
2022-09-26 08:44:14 -0600

    update Content-Security-Policy

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,7 @@
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve }) {
 	const response = await resolve(event);
-	response.headers.set('Content-Security-Policy', "frame-ancestors 'none'");
+	response.headers.set('Content-Security-Policy', "default-src 'self'");
 	response.headers.set('X-Content-Type-Options', 'nosniff');
 
 	return response;
