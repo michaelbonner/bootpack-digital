ddac9cc9a181c0e386541951e46e3d0c8986a591
Michael Bonner <michaelwbonner@gmail.com>
2022-09-26 08:50:58 -0600

    add more security headers

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,6 +4,8 @@ export const handle: Handle = async ({ event, resolve }) => {
 	const response = await resolve(event);
 	response.headers.set('Content-Security-Policy', "frame-src 'self'");
 	response.headers.set('X-Content-Type-Options', 'nosniff');
+	response.headers.set('X-Frame-Options', 'DENY');
+	response.headers.set('X-XSS-Protection', '1; mode=block');
 
 	return response;
 };
