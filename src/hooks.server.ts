de456252b3e06a8e94a84d4d8c004a0715052f3c
Michael Bonner <michaelwbonner@gmail.com>
2022-09-26 08:48:20 -0600

    update typescript on hooks.server.ts

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,9 @@
-/** @type {import('@sveltejs/kit').Handle} */
-export async function handle({ event, resolve }) {
+import type { Handle } from '@sveltejs/kit';
+
+export const handle: Handle = async ({ event, resolve }) => {
 	const response = await resolve(event);
-	response.headers.set('Content-Security-Policy', "default-src 'self'");
+	response.headers.set('Content-Security-Policy', "frame-src 'self'");
 	response.headers.set('X-Content-Type-Options', 'nosniff');
 
 	return response;
-}
+};
