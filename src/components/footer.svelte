492b3677189cc7ab3d0c9d59d7317eaa1c9d380e
Michael Bonner <michaelwbonner@gmail.com>
2023-05-30 17:04:45 -0600

    add footer padding for buttons

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -31,15 +31,15 @@
 </script>
 
 <footer class="items-center justify-between py-8 text-navy-100 lg:flex lg:py-0">
-	<nav class="w-full items-end justify-between p-4 text-sm md:p-8 lg:flex">
+	<nav class="w-full items-end justify-between p-4 pb-12 text-sm md:p-8 md:pb-24 lg:flex">
 		<div class="block items-center px-4 text-lg lg:flex lg:px-0">
-			<p class="mr-12 mb-4 text-sm lg:mb-0">
+			<p class="mb-4 mr-12 text-sm lg:mb-0">
 				&copy; {new Date().getFullYear()} Bootpack Digital, LLC
 			</p>
 			<div class="grid grid-cols-2 text-base lg:flex">
 				{#each links as link}
 					<a
-						class={`block font-semibold py-4 lg:py-2 lg:px-4 hover:underline decoration-2 hover:decoration-blue-300 transition-all
+						class={`block py-4 font-semibold decoration-2 transition-all hover:underline hover:decoration-blue-300 lg:px-4 lg:py-2
 						${$page.url.pathname === link.url && 'underline decoration-blue-600'}
 						`}
 						href={link.url}>{link.label}</a
