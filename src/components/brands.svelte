2ca55ef5a064095d4fa67d4d3debb2cb1f8cfb06
Michael Bonner <michaelwbonner@gmail.com>
2023-11-21 10:58:31 -0700

    add PACS to brands

--- src/components/brands.svelte
+++ src/components/brands.svelte
@@ -2,9 +2,9 @@
 	import CocaCola from '../images/clients/coca-cola-classic-gray.svg';
 	import EdgeHomes from '../images/clients/edge-homes-gray.svg';
 	import IntermountainHealthcare from '../images/clients/intermountain-healthcare-gray.svg';
-	import NationalEnergyFoundation from '../images/clients/national-energy-foundation-gray.svg';
 	import Ogio from '../images/clients/ogio-gray.svg';
 	import DominionEnergy from '../images/clients/dominion-energy-gray.svg';
+	import PACS from '../images/clients/pacs-gray.svg';
 	import BrandImage from './brand-image.svelte';
 </script>
 
@@ -23,22 +23,17 @@
 				</p>
 			</div>
 			<div class="mt-8 grid grid-cols-2 items-center gap-x-0.5 gap-y-4 md:grid-cols-3 lg:mt-0">
-				<BrandImage height={126} logo={CocaCola} title="CocaCola" width={300} />
-				<BrandImage
-					height={130}
-					logo={NationalEnergyFoundation}
-					title="National Energy Foundation"
-					width={300}
-				/>
-				<BrandImage height={65} logo={EdgeHomes} title="EDGEHomes" width={300} />
+				<BrandImage height={126} logo={CocaCola} title="CocaCola logo" width={300} />
+				<BrandImage height={33} logo={PACS} title="PACS logo" width={152} />
+				<BrandImage height={65} logo={EdgeHomes} title="EDGEHomes logo" width={300} />
 				<BrandImage
 					height={63}
 					logo={IntermountainHealthcare}
-					title="Intermountain Healthcare"
+					title="Intermountain Healthcare logo"
 					width={300}
 				/>
-				<BrandImage height={102} logo={Ogio} title="Ogio" width={300} />
-				<BrandImage height={150} logo={DominionEnergy} title="Dominion Energy" width={300} />
+				<BrandImage height={102} logo={Ogio} title="Ogio logo" width={300} />
+				<BrandImage height={150} logo={DominionEnergy} title="Dominion Energy logo" width={300} />
 			</div>
 		</div>
 	</div>
