7035094b66d5ba3a2dd579113f09e7e27d41cb8e
Michael Bonner <michaelwbonner@gmail.com>
2024-06-27 00:08:38 -0600

    add a little animation to the work tiles

--- src/components/work-item.svelte
+++ src/components/work-item.svelte
@@ -7,14 +7,16 @@
 	export let linkText = 'View Site';
 </script>
 
-<div class="flex flex-col overflow-hidden rounded-lg shadow-lg">
+<div
+	class="flex flex-col overflow-hidden rounded-lg shadow-lg transition-transform hover:-translate-y-1 hover:scale-[101%]"
+>
 	<a class="bg-blue-100" href={link} rel="noopener noreferrer" target="_blank">
 		<enhanced:img alt={title} loading={lazy ? 'lazy' : 'eager'} src={poster} />
 	</a>
 	<div class="flex flex-1 flex-col justify-between bg-white p-6">
 		<div class="flex-1">
 			<a class="block" href={link} rel="noopener noreferrer" target="_blank">
-				<h2 class="mt-2 text-xl font-semibold leading-7 text-gray-900">
+				<h2 class="mt-2 text-xl font-semibold leading-7 text-gray-900 hover:underline">
 					{title}
 				</h2>
 				<p class="mt-3 text-base leading-6 text-gray-500">
