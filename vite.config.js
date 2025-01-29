c7fc9a73ab8afb104fb28e6333fbac2d794956f1
Michael Bonner <michaelwbonner@gmail.com>
2025-01-29 09:48:45 -0700

    Update tailwind config to use css instead of js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,12 @@ import { join } from 'path';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { partytownVite } from '@builder.io/partytown/utils';
 import { enhancedImages } from '@sveltejs/enhanced-img';
+import tailwindcss from '@tailwindcss/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [
+		tailwindcss(),
 		sveltekit(),
 		enhancedImages(),
 		partytownVite({
