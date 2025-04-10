2e3a8cc7ec5971d8f7eeeb1e281a06335c1c6d0e
Michael Bonner <michaelwbonner@gmail.com>
2025-04-09 20:02:38 -0600

    Remove animated svg header logo

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -29,28 +29,11 @@
 </script>
 
 <header class="sticky top-0 z-20 py-2 px-6 mx-auto w-full bg-white shadow-md lg:shadow-none">
-	<div class="flex justify-between items-center w-full 2xl:px-4">
+	<div class="flex gap-2 justify-between items-center w-full 2xl:px-4">
 		<div class="flex justify-between items-center w-full">
-			<object
-				class={clsx('cursor-pointer h-16 -ml-4', 'lg:h-[80px] lg:py-0 lg:-my-2 lg:-ml-4')}
-				data="/images/bootpack-horizontal-animated.svg"
-				title="Bootpack Digital logo"
-				type="image/svg+xml"
+			<a class="py-2" href="/" title="Bootpack Digital home page">
+				<img alt="Bootpack Digital" height="45" width="218" src={BootpackHorizontal} /></a
 			>
-				<a
-					class={clsx('block -my-2', 'lg:mt-2 lg:mx-4')}
-					href="/"
-					title="Bootpack Digital home page"
-				>
-					<img
-						alt="Bootpack Digital"
-						class="py-2 h-14 lg:py-2 lg:h-18"
-						height="45"
-						width="218"
-						src={BootpackHorizontal}
-					/></a
-				>
-			</object>
 
 			<div class="hidden items-center lg:flex">
 				{#each mainLinks as link}
