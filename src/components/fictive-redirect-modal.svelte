77daad022f3158bba48f6534c10d152c7ee00ac6
Michael Bonner <mike@bootpackdigital.com>
2025-05-14 10:38:16 -0600

    Clicking overlay should close modal

--- src/components/fictive-redirect-modal.svelte
+++ src/components/fictive-redirect-modal.svelte
@@ -10,6 +10,11 @@
 			isVisible = true;
 		}
 	});
+
+	const closeModal = () => {
+		isVisible = false;
+		window.history.replaceState({}, '', window.location.pathname);
+	};
 </script>
 
 {#if isVisible}
@@ -21,16 +26,20 @@
 		in:fade={{ duration: 500 }}
 		out:fade={{ duration: 300 }}
 	>
-		<div class="fixed inset-0 bg-gray-500/75 transition-opacity" aria-hidden="true"></div>
+		<div
+			class="fixed inset-0 bg-gray-500/75 transition-opacity"
+			aria-hidden="true"
+			onclick={closeModal}
+		></div>
 
-		<div class="fixed inset-0 z-10 w-screen overflow-y-auto">
+		<div class="fixed inset-0 z-10 w-screen overflow-y-auto pointer-events-none">
 			<div
 				class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0"
 				in:fly={{ y: 40 }}
 				out:fly={{ y: -40 }}
 			>
 				<div
-					class="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6"
+					class="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6 pointer-events-auto"
 				>
 					<div>
 						<div
@@ -62,10 +71,7 @@
 						<button
 							type="button"
 							class="inline-flex w-full justify-center rounded-md bg-orange-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-orange-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600"
-							onclick={() => {
-								isVisible = false;
-								window.history.replaceState({}, '', window.location.pathname);
-							}}
+							onclick={closeModal}
 						>
 							Got it, thanks!
 						</button>
