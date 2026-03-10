e08bf214ff90e7ad3065bdb2fb60ace01267ec5c
Michael Bonner <michaelwbonner@gmail.com>
2026-03-10 00:03:10 -0600

    ignore eslint warning

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -57,11 +57,13 @@
 	<div class="grid gap-5 mx-auto mt-12 max-w-lg lg:grid-cols-3 lg:max-w-none">
 		{#each posts as post (post.slug)}
 			<div class="flex flex-col overflow-hidden rounded-lg shadow-lg">
+				<!-- eslint-disable-next-line svelte/no-navigation-without-resolve -->
 				<a aria-label={post.title} href={getBlogPostUrl(post.slug)} class="block shrink-0">
 					<enhanced:img src={post.thumbnail} alt={post.title} class="h-48 w-full object-cover" />
 				</a>
 				<div class="flex flex-col justify-between flex-1 p-6 bg-white">
 					<div class="flex-1">
+						<!-- eslint-disable-next-line svelte/no-navigation-without-resolve -->
 						<a href={getBlogPostUrl(post.slug)} class="block mt-2">
 							<p class="text-xl font-semibold text-gray-900">{post.title}</p>
 							<p class="mt-3 text-base text-gray-500">{post.description}</p>
